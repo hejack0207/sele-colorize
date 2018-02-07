3d2a065bffc38686365bed65abebab33fc7f9a81
hejack0207 <hejack0207@sina.com>
2018-02-07 12:53:04 +0800

    fix

--- scolor/main.go
+++ scolor/main.go
@@ -20,15 +20,17 @@ func main() {
 	var (
 		start    string
 		end      string
+		pos      string
 		offset   int
 		filepath string
 	)
 	flag.StringVar(&start, "start", "1,0", "start position of selection")
 	flag.StringVar(&end, "end", "1,0", "end position of selection")
 	flag.IntVar(&offset, "offset", 0, "offset of bytes in file")
+	flag.StringVar(&pos, "pos", "1,0", "position in input file")
 	flag.StringVar(&filepath, "f", "", "file to print")
 
 	flag.Parse()
-	strings.Split(start, ",")
-	//fmt.Printf("start:%s,end:%s", start, end)
+	lc := strings.Split(pos, ",")
+	fmt.Printf("line:%s,column:%s", lc[0], lc[1])
 }
